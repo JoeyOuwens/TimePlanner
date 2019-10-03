bd20c78c4b8fa5a1fba3338225276a9cb7327c60
Joey Ouwens <Joey.Ouwens@gmail.com>
2019-10-03 16:36:52 +0200

    no message

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
-});
+var users = [{ id: 1, name: 'Jack the ripper', function: 'Wasser', fulltime: true, email: "jackyboy@gmail.com", hours: 168, dob: "7-7-1888", rights: "manager" },
+               { id: 2, name: 'Elon Musk', function: 'Legend', fulltime: true, email: "emusk@spacex.com", hours: 168, dob: "29-6-1971", rights: "medewerker" }]; 
+var userHasPageAccess = true;
+
+/* GET users page. */
+if (userHasPageAccess) {
+    router.get('/', function (req, res) {
+        res.render('users', { title: 'Medewerkers', users: users });
+    });
+} else {
+    router.get('/', function (req, res) {
+        return res.redirect('/');
+    });
+}
+
+
 
 module.exports = router;
