b3d3fc43507224a65c39f67c012e7f139803f4e1
Joey Ouwens <Joey.Ouwens@gmail.com>
2019-10-11 01:10:44 +0200

    no message

--- routes/usermanagement/list.js
+++ routes/usermanagement/list.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
-var users = [{ id: 1, name: 'Jack the Ripper', function: 'Wasser', fulltime: true, email: "jackyboy@gmail.com", hours: 168, dob: "7-7-1888", rights: "manager" },
-               { id: 2, name: 'Elon Musk', function: 'Legend', fulltime: true, email: "emusk@spacex.com", hours: 168, dob: "29-6-1971", rights: "medewerker" }]; 
+var users = [{ id: 1, name: 'Jack the Picker', function: 'Wasser',  email: "jackyboy@gmail.com", hours: 168, dob: "7-7-1888", rights: "manager" },
+    { id: 2, name: 'Elon Musk', function: 'Entrepreneur', email: "emusk@spacex.com", hours: 168, dob: "29-6-1971", rights: "medewerker" },
+    { id: 3, name: 'Paul Herbert', function: 'Ober', email: "PHerbert@gmail.com", hours: 18, dob: "12-4-1997", rights: "medewerker" }]; 
 var userHasPageAccess = true;
 var userRights = "OWNER";
 
