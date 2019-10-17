b3caa9f8d7615773dac056096d799c82435ba9bd
Joey Ouwens <Joey.Ouwens@gmail.com>
2019-10-17 13:06:54 +0200

    field validation

--- routes/usermanagement/create.js
+++ routes/usermanagement/create.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var emailHandler = require('../../classes/emailHandler');
 var generatePassword = require('password-generator');
+var validation = require('../../classes/validation');
 var router = express.Router();
 
 
@@ -13,8 +14,8 @@ var insertedIntoDB = true;
 
 /* GET create user page. */
 if (userHasPageAccess) {
-    router.get('/', function (req, res) {
-        res.render('usermanagement/create', { title: 'Nieuwe medewerker', userRights : userRights });
+    router.get('/', function (req, res) { 
+        res.render('usermanagement/create', { title: 'Nieuwe medewerker', userRights : userRights, newPage: true });
     });
 } else {
     router.get('/', function (req, res) {
@@ -27,17 +28,64 @@ router.post('/', function (req, res) {
 
     var password = generatePassword(Math.floor(Math.random() * 10) + 8, false);
     var accountDetails = req.body;
-    if (validationPassed) {
+
+    accountDetails.inputZipcode = accountDetails.inputZipcode.replace(/\s/g, ''); 
+    var failedFields = fieldValidation(accountDetails);
+    if (failedFields.length == 0) {
         if (insertedIntoDB) {
             emailHandler.sendNewAccountEmail(accountDetails.inputEmail, accountDetails.inputFname, password);
-        }     
-    }
+            handlePageStayCheck(req.body.checkPageStay,res)
+        }        
+    } else {
+        res.render('usermanagement/create', { title: 'Nieuwe medewerker', userRights: userRights, failedFields: failedFields, accountDetails: accountDetails });
+    }        
+}); 
 
-    if (req.body.checkPageStay) {
+function handlePageStayCheck(checkPageStay,res) {
+    if (checkPageStay) {
         return res.redirect('/usermanagement/create');
     } else {
         return res.redirect('/usermanagement/list');
     }
-});
+}
+
+function fieldValidation(details) {
+    var failed = [];
+    if (!validation.zipcode(details.inputZipcode)) {
+        failed.push("inputZipcode");
+    }
+    if (!validation.birthdate(details.inputDOB)) {
+        failed.push("inputDOB");
+    }
+    if (!validation.hours(details.inputWorkHours)) {
+        failed.push("inputWorkHours");
+    }
+    if (!validation.rights(details.inputRights)) {
+        failed.push("inputRights");
+    }
+    if (!validation.telephone(details.inputTelephone)) {
+        failed.push("inputTelephone");
+    }
+    if (!validation.email(details.inputEmail)) {
+        failed.push("inputEmail");
+    }
+    if (!validation.place(details.inputPlace)) {
+        failed.push("inputPlace");
+    }
+    if (!validation.address(details.inputAddress)) {
+        failed.push("inputAddress");
+    }
+    if (!validation.firstName(details.inputFname)) {
+        failed.push("inputFname");
+    }
+    if (!validation.middleName(details.inputMname)) {
+        failed.push("inputMname");
+    }
+    if (!validation.lastName(details.inputLname)) {
+        failed.push("inputLname");
+    }
+    return failed;
+     
+};
 
 module.exports = router;
