22b1f05823626d9201c6ebd2dd42b449aff172b4
rudhy95 <r.udhaya95@gmail.com>
2019-10-31 23:22:56 +0100

    Profile Name Render in Profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -4,9 +4,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('profile', { error: true });
+    res.render('profile', { error: true, username: req.session.user.firstname + ' ' + req.session.user.lastname});
 
-    res.render('users', { username: req.session.user.firstname + ' ' + req.session.user.lastname })
 });
 
 module.exports = router;
