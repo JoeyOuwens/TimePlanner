c89d9210573be7d8dde32354c97662780dc0015f
Albert <0961988@hr.nl>
2019-11-08 13:52:57 +0100

    HotFix header issues

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('dashboard', { error: true, username: req.session.user.firstname + ' ' + req.session.user.lastname });
+    res.render('dashboard', { error: true, username: req.session.user.firstname + ' ' + req.session.user.lastname, hidenav: false });
 });
 
 module.exports = router;
