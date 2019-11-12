10029b0b4bcde1e403d71e53ec3bd9d1492c0693
Albert <0961988@hr.nl>
2019-11-12 13:19:50 +0100

    added dynamic time to seed timetable_items

--- seeds/timetable_items.js
+++ seeds/timetable_items.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
         return knex('timetable_items').insert([
-          { user: 1, begin_date: '2020-02-22 12:00:00', end_date: '2020-02-22 14:00:00', comment: 'Komt regelmatig te laat, let hier op. '},
-          { user: 2, begin_date: '2020-02-22 12:00:00', end_date: '2020-02-22 14:00:00', comment: ''},
-          { user: 1, begin_date: '2020-02-22 12:00:00', end_date: '2020-02-22 14:00:00', comment: ''} 
-      ]);
+            { user: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: 'Komt regelmatig te laat, let hier op. '},
+            { user: 2, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''},
+            { user: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''} 
+        ]);
     });
 };
