c5f2782035931c1c23fe7fd077e75bc77616ec0f
canberkates <45592566+canberkates@users.noreply.github.com>
2019-11-29 01:17:28 +0100

    evencreationcheckupdate

--- routes/rooster.js
+++ routes/rooster.js
@@ -31,13 +31,19 @@ router.post('/add-user', async function (req, res, next) {
     if (req.session.user.role === 'USER') 
         res.redirect('/rooster/');
 
-    await knex('timetable_items').insert([
-        {
-            user: req.body.user,
-            begin_date: req.body.begin_date,
-            end_date: req.body.end_date,
-            comment: req.body.comment
-        }]);
+    if (req.session.begin_date >= req.session.end_date) {
+        res.render('add-user');
+    } else {
+        await knex('timetable_items').insert([
+            {
+                user: req.body.user,
+                begin_date: req.body.begin_date,
+                end_date: req.body.end_date,
+                comment: req.body.comment
+            }]);
+    }
+
+    
 
     if (req.body.stay_on_page === 'on')
         res.render('rooster/add-user', { title: "Gebruiker inroosteren", user_list: await userDBHandler.getAllUsers(), success: true });
