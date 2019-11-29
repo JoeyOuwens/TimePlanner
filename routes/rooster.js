3d72b4ed8bd703ac204c6769a203caf6b23651ea
canberkates <45592566+canberkates@users.noreply.github.com>
2019-11-29 13:26:54 +0100

    update

--- routes/rooster.js
+++ routes/rooster.js
@@ -30,13 +30,18 @@ router.post('/add-user', async function (req, res, next) {
     if (req.session.user.role === 'USER') 
         res.redirect('/rooster/');
 
-    await knex('timetable_items').insert([
-        {
-            user: req.body.user,
-            begin_date: req.body.begin_date,
-            end_date: req.body.end_date,
-            comment: req.body.comment
-        }]);
+    if (req.body.begin_date >= req.body.end_date)
+        res.render('rooster/add-user', { title: "Gebruiker inroosteren", user_list: await userDBHandler.getAllUsers(), error: "Begintijd mag niet later zijn dan eindtijd", success: false });
+    else
+        await knex('timetable_items').insert([
+            {
+                user: req.body.user,
+                begin_date: req.body.begin_date,
+                end_date: req.body.end_date,
+                comment: req.body.comment
+            }]);
+    
+
 
     if (req.body.stay_on_page === 'on')
         res.render('rooster/add-user', { title: "Gebruiker inroosteren", user_list: await userDBHandler.getAllUsers(), success: true });
