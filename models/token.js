a403aa1db178be6a8fd5f6cc5af191274981d01e
Albert <seta@live.nl>
2019-12-17 20:39:23 +0100

    Added relationmapping of user to the token

--- models/token.js
+++ models/token.js
@@ -1,11 +1,30 @@
 const { Model } = require('objection');
 const knex = require('../db/knex');
+var crypto = require("crypto");
 
 Model.knex(knex);
 
 class Token extends Model {
     static get tableName() {
         return 'token';
+    }
+
+    static get relationMappings() {
+        return {
+            user: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: require("./user"),
+                join: {
+                    from: 'token.user_id',
+                    to: 'users.id'
+                }
+            }
+        };
+    }
+
+    generateToken() {
+        this.token_serial = crypto.randomBytes(15).toString('hex');
+        return this.token_serial;
     }
 }
 
