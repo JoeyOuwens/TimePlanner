8f1d603f3357f84d374f226efa428be31836bdb2
Albert <seta@live.nl>
2019-12-17 16:02:54 +0100

    Added id's to the timetable seeds

--- seeds/timetable_items.js
+++ seeds/timetable_items.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
         return knex('timetable_items').insert([
-            { user_id: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: 'Komt regelmatig te laat, let hier op. '},
-            { user_id: 2, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''},
-            { user_id: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''} 
+            { id: 1, user_id: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: 'Komt regelmatig te laat, let hier op. '},
+            { id: 2, user_id: 2, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''},
+            { id: 3, user_id: 1, begin_date: new Date().toLocaleString(), end_date: new Date(new Date().setHours(new Date().getHours() + 2)).toLocaleString(), comment: ''} 
         ]);
     });
 };
