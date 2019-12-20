ab8e6e51e9cb43001fa15af1029fdbfb005b70b7
Timvdperk <timvdperk@gmail.com>
2019-12-20 12:18:08 +0100

    back button removed from popup on first one

--- public/javascript/modal.js
+++ public/javascript/modal.js
@@ -20,9 +20,11 @@ $("#back-btn").click(function () {
 function changeContext() {
     switch (page) {
         case 1:
+            $("#back-btn").hide();
             $('#bodyText').html("<p>Bovenin  is de navigatie balk. Deze word gebruikt om vrij te vragen, vervanging te regelen, het profiel in te zien, of om de roosters te bekijken.</p> ");
             break;
         case 2:
+            $("#back-btn").show();
             $('#bodyText').html("<p>Bij het klikken op home kom je op de homepage terecht, hier zie je je rooster voor aankomende week, notificaties, en de nieuwste medewerkers </p> ");
             break;
         case 3:
