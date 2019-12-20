780daf54f062a6fb0330c9576d5760b7974cc55f
canberkates <45592566+canberkates@users.noreply.github.com>
2019-12-20 01:43:55 +0100

    Update substitute.js

--- routes/roostermanagement/substitute.js
+++ routes/roostermanagement/substitute.js
@@ -1,5 +1,4 @@
 'use strict';
-const substitute = require('../../models/substitute');
 var knex = require('../../db/knex');
 
 class RequestSubstitute {
@@ -11,15 +10,18 @@ class RequestSubstitute {
         let timetableUser = await knex.select('timetable_items.user')
             .from("timetable_items").where('id', id);
         if (timetableUser[0].user == req.session.user.id) {
-            await knex('substitute').insert([
-            {
-                requesting_user: req.session.user.id,
-                replaced_by_user: req.session.user.id,
-                creation_datetime: new Date(),
-                timetable_item: req.body.id,
-                status: 'AWAITING_REPLACEMENT',
-                comment:''
-                }]).catch(function (e) { console.log(e) });
+            let substitute = await knex.select().from('substitute').where('timetable_item', id);
+            if (substitute.length == 0) {
+                await knex('substitute').insert([
+                    {
+                        requesting_user: req.session.user.id,
+                        replaced_by_user: req.session.user.id,
+                        creation_datetime: new Date(),
+                        timetable_item: req.body.id,
+                        status: 'AWAITING_REPLACEMENT',
+                        comment: ''
+                    }]).catch(function (e) { console.log(e) });
+            }
         }
        
     }
