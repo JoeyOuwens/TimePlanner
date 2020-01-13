f92ca6b3ec0ebf7b2792d7670a26d5072d902456
Joey Ouwens <joey.ouwens@gmail.com>
2020-01-13 18:05:16 +0100

    Users gets added to the rooster list Rooster items are displayed

--- routes/roostermanagement/index.js
+++ routes/roostermanagement/index.js
@@ -1,27 +1,72 @@
 'use strict';
 var TimeTableItems = require('../../models/timetable_item');
-
+var SickDays = require('../../models/sickday');
+//var Substitute = require('../../models/substitute');
+var SICK_COLOR = "purple";
+var TAKEN_OVER_COLOR = "green"; 
+var REQUESTING_SUBSTITUTE_COLOR = "orange"; 
+var DEFAULT_COLOR = "";
+// ToDo color meegeven als hij overgenomen is.
 class Index {
     static async get(req, res, next) {
         let timetable_list = [];
+        let resource_list = [];
 
         await TimeTableItems.query().eager('user').then((items) => {
             items.forEach((item) => {
+                resource_list.push(
+                    {
+                        "id": item.user.id,
+                        "title": item.user.getFullName()
+                    }); 
+
                 timetable_list.push(
                     {
-                        "title": item.user.getFullName(),
+                        "title": createEventTitle(item),
                         "start": item.begin_date,
                         "end": item.end_date,
-                        "timetable_id": item.id
+                        "id": item.id,
+                        "resourceId": item.user.id,
+                        "color": item.user.id == 1? SICK_COLOR : "blue"
+                        
                     });
             });
         }); 
         res.render('rooster',
             {
                 title: 'Rooster',
-                timeTable: JSON.stringify(timetable_list)
+                timeTable: JSON.stringify(timetable_list),
+                resources: JSON.stringify(resource_list)
             });
     }
 }
 
 module.exports = Index;
+
+
+function createEventTitle(item) {
+    //Creates a title with HH:MM - HH:MM
+    return `${new Date(item.begin_date).getHours()}:${String(new Date(item.begin_date).getMinutes()).length == 1 ? "0" + String(new Date(item.begin_date).getMinutes()) : String(new Date(item.begin_date).getMinutes())} - ${new Date(item.end_date).getHours()}:${String(new Date(item.end_date).getMinutes()).length == 1 ? "0" + String(new Date(item.end_date).getMinutes()) : String(new Date(item.end_date).getMinutes())}`;
+
+
+}
+
+function isUserSick(users, sickDays) {
+
+}
+
+function substituteHandling() {
+
+
+}
+
+function isStillRequestingSubstitute() {
+
+
+}
+
+function isTakenOver() {
+
+
+
+}
