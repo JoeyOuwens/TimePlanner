882de8f8dfc9020813732b14ccbe41b56a80fae3
Albert <0961988@hr.nl>
2020-01-15 00:29:31 +0100

    Offine route uses the functions from the rooster pagem removed redundant code usage

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var Token = require('../models/token');
 var Users = require('../models/user');
 const TimeTableItems = require('../models/timetable_item');
+const rooster_index = require('./roostermanagement/index');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -22,20 +23,6 @@ router.get('/offline', async function (req, res) {
     if (req.session.user !== undefined)
         context.timetable_days = await res.locals.userInfo.get_week_scedule();
 
-    var resource_list = [];
-    await Users.query().where("active", "=", true)
-        .then((users) => {
-            users.forEach((user) => {
-                resource_list.push({
-                    "id": user.id,
-                    "title": user.getFullName()
-                });
-            });
-        });
-
-    context.resources = JSON.stringify(resource_list);
-
-    var timetable_list = [];
     let first_day_of_week = new Date();
     first_day_of_week.setHours(0, 0, 0, 0);
     first_day_of_week.setDate(first_day_of_week.getDate() - first_day_of_week.getDay() + (first_day_of_week.getDay() === 0 ? -6 : 1));
@@ -46,29 +33,9 @@ router.get('/offline', async function (req, res) {
     console.log(first_day_of_week);
     console.log(first_day_of_next_week);
 
-    await TimeTableItems.query().eager('user')
-        .where('begin_date', '>=', first_day_of_week.toISOString().replace('T', ' ').replace('Z', ''))
-        .where('end_date', '<=', first_day_of_next_week.toISOString().replace('T', ' ').replace('Z', ''))
-        .then(async (items) => {
+    context.resources = JSON.stringify(await rooster_index.getResourceList());
 
-            for (let item of items) {
-
-                timetable_list.push(
-                    {
-                        "title": createEventTitle(item),
-                        "start": item.begin_date,
-                        "end": item.end_date,
-                        "id": item.id,
-                        "resourceId": item.user.id,
-                        "comment": item.comment,
-                        "user_id": item.user.id,
-                        "color": "#00aedb"
-                    });
-            };
-        });
-
-
-    context.events = JSON.stringify(timetable_list);
+    context.events = JSON.stringify(await rooster_index.getTimeTableData(first_day_of_week, first_day_of_next_week));
 
     res.render('offline', context);
 });
