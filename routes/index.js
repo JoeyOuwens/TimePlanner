b67fe20b1b4d0e64d561b4654d67011b0fb639a0
Albert <0961988@hr.nl>
2020-01-14 23:14:43 +0100

    Changed schedule request to user function, seperated user week rooster view to different file

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,11 @@ router.get('/', function (req, res, next) {
 
 /* GET offline page. */
 router.get('/offline', async function (req, res) {
-    //res.sendFile('public/offline.html');
-
 
     var context = { title: 'Offline Rooster' };
 
     if (req.session.user !== undefined)
-        context.timetable_days = await get_scedule_for_user(req.session.user.id);
+        context.timetable_days = await res.locals.userInfo.get_week_scedule();
 
     var resource_list = [];
     await Users.query().where("active", "=", true)
@@ -45,8 +43,8 @@ router.get('/offline', async function (req, res) {
     first_day_of_next_week.setHours(0, 0, 0, 0);
     first_day_of_next_week.setDate(first_day_of_week.getDate() + 7);
 
-    console.log(first_day_of_week)
-    console.log(first_day_of_next_week)
+    console.log(first_day_of_week);
+    console.log(first_day_of_next_week);
 
     await TimeTableItems.query().eager('user')
         .where('begin_date', '>=', first_day_of_week.toISOString().replace('T', ' ').replace('Z', ''))
@@ -74,55 +72,6 @@ router.get('/offline', async function (req, res) {
 
     res.render('offline', context);
 });
-
-
-async function get_scedule_for_user(userid) {
-    const week_day_name = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
-    var begin_date = new Date();
-    var end_date = new Date();
-    let days = [];
-
-    begin_date.setHours(0, 0, 0, 0);
-    end_date.setDate(end_date.getDate() + 8);
-    end_date.setHours(0, 0, 0, 0);
-    /* Get the upcomming 7 days that the user is sceduled to work and add new values to the user model that contain their start and end hours/minutes and add it to the items variable */
-    let items = await TimeTableItems.query()
-        .where('user_id', userid)
-        .where('begin_date', '>=', begin_date.toISOString().replace('T', ' ').replace('Z', ''))
-        .where('end_date', '<=', end_date.toISOString().replace('T', ' ').replace('Z', '')).then((values) => {
-            values.forEach((item, index, array) => {
-                item.begin_time = new Date(item.begin_date).toTimeString().split(' ')[0].split(/(.+):/)[1];
-                item.end_time = new Date(item.end_date).toTimeString().split(' ')[0].split(/(.+):/)[1];
-                array[index] = item;
-            });
-            return values;
-        });
-
-    /* For loop for the upcomming 7 days that will be displayed on the dashboard */
-    for (let i = 0; i < 7; i++) {
-        /* Create today and tomorrow variables */
-        var current_date = new Date();
-        var next_date = new Date();
-
-        /* Set correct dates for the variables*/
-        current_date.setDate(current_date.getDate() + i);
-        current_date.setHours(0, 0, 0, 0);
-        next_date.setDate(next_date.getDate() + i + 1);
-        next_date.setHours(0, 0, 0, 0);
-
-        /* Add day X object to the days array with the name of the week */
-        days[i] = { day_name: week_day_name[current_date.getDay()], day_count: i, day_items: [] };
-
-        /* Loop through all timetable entries and check if their day is  */
-        for (let j = 0; j < items.length; j++) {
-            let item = items[j];
-            if (Date.parse(item.begin_date) >= current_date.getTime() && Date.parse(item.begin_date) <= next_date.getTime()) {
-                days[i].day_items.push(item);
-            }
-        }
-    }
-    return days;
-}
 
 
 function createEventTitle(item) {
